97de6d381f215a6bcd564656ab029b4b6e661de1
Flynn25 <Kayla425@users.noreply.github.com>
2017-10-04 07:37:34 -0400

    Add files via upload

--- contactsend.php
+++ contactsend.php
@@ -7,14 +7,14 @@ if ($_POST['submit']) {
   $name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
-  $to = "animalhousevermont@gmail.com";
+  $to = 'animalhousevermont@gmail.com';
   $subject = "";
 
   if (mail($to, $subject, $message)){
-    mail ($to,$subject,$message);
-    echo '<p>message sent!</p>';
+      mail ($to,$subject,$message);
+      echo '<p>message sent!</p>';
   } else {
-    echo '<p>something went wrong!</p>';
+      echo '<p>something went wrong!</p>';
   }
 }
 ?>
